4bf8158c3b7caf758dc40ff9669ad3c4012ad888
Naman Yadav <yadavnaman761@gmail.com>
2025-08-19 00:34:20 +0530

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV != "production"){
+require('dotenv').config();
+console.log(process.env.SECRET);
+}
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
@@ -11,6 +15,7 @@ const { listingSchema, reviewSchema }= require("./schema.js");
 const Review = require("./models/review.js");
 const { appendFileSync } = require("fs");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash=require("connect-flash");
 const passport= require("passport");
 const LocalStrategy = require("passport-local");
@@ -21,8 +26,7 @@ const userRouter=require("./routes/user.js");
 
 
 
-
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl=process.env.ATLASDB_URL;
 main()
 .then(()=>{
     console.log("connecterd to DB");
@@ -32,7 +36,7 @@ main()
 });
 
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -42,9 +46,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET
+    },
+    touchAfter: 24*3600,
+});
+   
+store.on("error", () => {
+    console.log("ERROR in MONGO SESSION STORE", err);
+});
 
 const sessionOptions={
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized:true,
     cookie:{
@@ -54,9 +70,10 @@ const sessionOptions={
     }
 };
 
-app.get("/",(req,res)=>{
-    res.send("Hi,I am root");
-});
+//app.get("/",(req,res)=>{
+  //  res.send("Hi,I am root");
+//});
+
 
 
 app.use(session(sessionOptions));
@@ -90,8 +107,8 @@ app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/",userRouter);
 
-
-
+   
+        
 
 app.all(/.*/, (req, res, next) => {
            next(new ExpressError(404, "Page Not Found"));
--- models/listing.js
+++ models/listing.js
@@ -9,11 +9,8 @@ const listingSchema=new Schema({
     } ,
     description: String,
     image: {
-  filename: String,
-  url: {
-    type: String,
-    default: "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-  }
+        url: String,
+        filename:String,    
 },
  
     price: Number,
